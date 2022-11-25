8b465c9366d25f707d4cd692ff4fbe4ead933668
jaime1315321 <jamoncayop@unal.edu.co>
2022-11-25 07:19:05 -0500

    maquetación completa

--- src/components/BodyPortafolio.jsx
+++ src/components/BodyPortafolio.jsx
@@ -3,6 +3,7 @@ import styles from '../styles/BodyPortafolio.module.css'
 import  mayor_morado from '../assets/mayor_morado.svg' 
 import  portafolio from '../assets/portafolio.svg' 
 import btn_next from '../assets/btn_next.svg'
+import btn_descarga from '../assets/iconoDescarga.svg'
 import { Proyectos } from './Proyectos'
 
 
@@ -30,7 +31,10 @@ export const BodyPortafolio = () => {
                         <div className={`${styles.contenedorHorizontal} ${styles.contenedorBotones}`}  >
                             <button className={`${styles.btn_contact}`} >Contact me!!</button>
 
-                            <button className={`${styles.btn_download}`}  > Dowloand </button>
+                            <button className={`${styles.btn_download}`}  >
+                                 Dowloand CV
+                                 <img className={`${styles.btn_download2}`}   src={btn_descarga} alt="" />
+                                  </button>
                         </div>
                         <img className={styles.iconoDown} src={btn_next} alt="" />
 
